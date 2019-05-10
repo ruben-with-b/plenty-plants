af86ead3ab4fd6a334a46807d8db12f7b4093100
Rudi Loderer <rudi.loderer@hs-augsburg.de>
2019-05-10 13:14:32 +0200

    Add login-component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 // import components
-// import Login from './components/Login.vue'
+import Login from './components/Login.vue'
 // import Menu from './Menu.vue'
 // import About from './About.vue'
-import HelloWorld from "./components/HelloWorld";
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter);
@@ -15,11 +14,8 @@ require("./styles/main.scss");
 
 const routes = [
   { path: '/',
-    component: HelloWorld
-  }/*,
-  { path: '/login',
      component: Login
-  },
+  }/*,
   { path: '/menu',
     component: Menu
   },
