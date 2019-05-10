d199b22e03b70a3f8728bbcb923a4150e2f02225
Rudi Loderer <rudi.loderer@hs-augsburg.de>
2019-05-10 13:06:30 +0200

    Fix router setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,20 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
-/*
-import Menu from './Menu.vue'
-import About from './About.vue'*/
+// import components
+// import Login from './components/Login.vue'
+// import Menu from './Menu.vue'
+// import About from './About.vue'
+import HelloWorld from "./components/HelloWorld";
 import VueRouter from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 
-require("./styles/main.scss")
+require("./styles/main.scss");
 
 const routes = [
   { path: '/',
-    component: App
+    component: HelloWorld
   }/*,
+  { path: '/login',
+     component: Login
+  },
   { path: '/menu',
     component: Menu
   },
@@ -26,7 +31,7 @@ const routes = [
 const router = new VueRouter({
   routes, // routes: routes
   mode: 'history'
-})
+});
 
 new Vue({
   render: h => h(App),
